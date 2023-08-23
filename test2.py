019921dc16596e2e24570a48ea680e3e73af7401
prathamkandari <prathamkandari123@gmail.com>
2023-08-23 10:27:39 +0530

    New

---
+++ test2.py
@@ -0,0 +1,28 @@
+import scrapetube
+import pandas as pd
+from comments_youtube import ScrapComment
+# from comment_date import comment_date
+
+channelid="UCl_1ZH0cUIFXGh9Tj0A47DA"
+list=[]
+
+url="https://www.youtube.com/watch?v="
+
+dataframe = pd.DataFrame(columns=["URL"])
+
+videos = scrapetube.get_channel(channelid)
+
+for video in videos:
+    url1 = url + str(video['videoId'])
+    list.append(url1)
+    # comment_date(url1)
+    print(url1)
+
+    # ScrapComment(url1)
+
+# print(list)
+
+dataframe['URL'] = list
+dataframe.to_excel("MYURL.xlsx")
+
+# videos=scrapetube.get_channel(channelid)
